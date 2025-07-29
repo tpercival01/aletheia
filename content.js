cce3f80344b2f5eba8dd065a079d8fcda2f83961
tpercival01 <thomaspercival@live.co.uk>
2025-07-29 17:31:46 +0100

    Adding comments to setup for implementation

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
+// Await page to loaded fully before grabbing
 const webpage_html = document.documentElement.innerHTML;
 
+// Implement various data scraping techniques to get all large bodies of text, all images, all videos, and all audios.
+// Implement mutations to check for dynamically loaded content.
+
 chrome.runtime.sendMessage({ type: "PAGE_LOADED_HTML", html: webpage_html });
+
+// Listen out for messages from background.js for various UI changes
+
+// Listen for messages from popup.js for rescans
